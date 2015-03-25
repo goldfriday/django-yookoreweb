66119db892a32aa7a4b09e0f1e9af14318d0b25f
= <=>
2015-03-25 07:50:58 +0200

    Implementation of the login, => stream

--- frontend/views.py
+++ frontend/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+
+import json
+import requests
 
 # Create your views here.
 
@@ -13,3 +17,28 @@ def activitystream(request):
 def userprofile(request):
     context = {}
     return render(request, 'frontend/views/profile.html', context)
+
+def yookore_login(request):
+
+	if request.method == 'POST':
+
+		url = "http://192.168.10.144:3000/auth/login"
+		headers = {'content-type': 'application/json'}
+
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+
+		params = {
+			"username": username,
+			"password":password
+		}
+		print params
+		response = requests.post(url, data=json.dumps(params), headers=headers)
+		if response:
+			data = response.json()
+			print data
+			context = {}
+			context['session_d'] = data['sessionid']
+			return render(request, 'frontend/views/stream.html', context)
+		else:
+			return render(request, '.')
