e39c41269b4b1f408f96a3af2c0d44d4958d594a
Kofi Oroh <kofioroh@gmail.com>
2015-04-08 10:52:25 +0200

    Merge

--- userprofile_app/views.py
+++ userprofile_app/views.py
@@ -36,3 +36,68 @@ def get_user_profile(username):
     except:
         print 'An error occured'
         pass
+
+
+def update_user_profile(request):
+    context = {}
+    username = 'ptchankue'
+    context ['username'] = username
+    context['profile_info'] = get_user_profile('ptchankue')
+    if request.method == 'GET':
+        print 'calling user profile'
+        url = "http://192.168.10.144:3000/auth/profile/" + username
+        print url
+        headers = {'content-type': 'application/json'}
+        try:
+            response = request.get(url, headers=headers)
+            if response:
+                data = response.json()
+                print data
+                return data
+            else:
+                return 'error'
+        except:
+            print 'An error occured'
+
+        return render(request, 'frontend/views/update_profile.html', context)
+
+    if request.method == 'PUT':
+        print 'calling user profile update url'
+        url = "http://192.168.10.144:3000/auth/profile/update" + username
+        headers = {'content-type': 'application/json'}
+
+        params = {
+            "birthdate": birthdate,
+            "gender": gender,
+            "title": title,
+            "password": password,
+            "relationshipstatus": relationshipstatus,
+            "timezone": timezone,
+            "homecountry": homecountry,
+            "imageurl": imageurl,
+            "alternate_email": alternate_email,
+            "religion": religion,
+            "cellphone": cellphone,
+            "biography": biography
+        }
+        # print params
+        error = False
+        try:
+
+            response = requests.put(url, data=json.dumps(params), headers=headers)
+        except:
+            print 'unable to update user profile'
+            error = True
+            return render(request, '.')
+        if not error:
+            data = response.json()
+            # print data
+            context = {
+                "username": data['username'],
+                "sessionid": data['sessionid'],
+                "fullname": data['fullname']
+            }
+
+            return render(request, 'frontend/views/update_profile.html', context)
+        else:
+            return render(request, '.')
